6b7f819a44d4fac34793b18ed921c97d2e4ac05e
Korisss <koriss@vk.com>
2025-12-07 10:43:38 +0300

    feat: make neovim default editor

--- link.sh
+++ link.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-stow -R -v -t ~ ./makepkg
-stow -R -v -t ~ ./yandex
-stow -R -v -t ~ ./zsh
+stow -R -v -t ~ makepkg
+stow -R -v -t ~ nvim
+stow -R -v -t ~ yandex
+stow -R -v -t ~ zsh
 
